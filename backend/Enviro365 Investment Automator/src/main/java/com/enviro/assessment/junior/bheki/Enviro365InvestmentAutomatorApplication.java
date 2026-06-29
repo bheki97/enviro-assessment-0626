252e4bc44b3e5e37834e753b032d470faaad2da5
bheki97 <bfmokoena7@gmail.com>
2026-06-29 09:39:05 +0200

    Springboot base application

---
+++ backend/Enviro365 Investment Automator/src/main/java/com/enviro/assessment/junior/bheki/Enviro365InvestmentAutomatorApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.bheki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365InvestmentAutomatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Enviro365InvestmentAutomatorApplication.class, args);
+    }
+
+}
